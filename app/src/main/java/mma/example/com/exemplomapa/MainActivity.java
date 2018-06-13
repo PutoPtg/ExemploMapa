dc6261b8e495b89b3d3e9c2cba21431750c0fd84
Manuel Amado <manuelamado@Testss-MacBook-Air.local>
2018-06-13 10:01:49 +0100

    first comit

---
+++ app/src/main/java/mma/example/com/exemplomapa/MainActivity.java
@@ -0,0 +1,13 @@
+package mma.example.com.exemplomapa;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
