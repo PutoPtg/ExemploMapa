914ebcaf0f052480fbe8d423ab72498869d8636d
Manuel Amado <manuelamado@iclio.pt>
2018-06-14 14:53:32 +0100

    github first commit

--- app/src/main/java/mma/example/com/exemplomapa/MainActivity.java
+++ app/src/main/java/mma/example/com/exemplomapa/MainActivity.java
@@ -35,26 +35,24 @@ public class MainActivity extends Activity {
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN); //remove barra de notificação
         setContentView(R.layout.activity_main); //Colocar após os dois anteriores para evitar crashes
 
-        switchGPS = (Switch)findViewById(R.id.switch1);
         //inicializar variáveis
         myVariables = new VariablesClass(0,0);
+        switchGPS = (Switch)findViewById(R.id.switch1);
 
-
-
-        // Zona para pedir autorizações aos utilizadores
 
         //passo 1 - Verificar a versão do SDK
         if (android.os.Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
             verifyPermitions();
         }else{
-            //TODO LAUNCH MENU
+            switchGPS.setSaveEnabled(false);
+            Intent menuActivity = new Intent(this, MainMenu.class);
+            startActivity(menuActivity);
         }
     }
 
 
 
     private void verifyPermitions(){
-
 
         //passo 2 - Verificar permissões
         if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
@@ -69,7 +67,8 @@ public class MainActivity extends Activity {
             //se sim, tem permissão e pode arrancar o mapa
 
             switchGPS.setChecked(true);
-            //TODO criar e chamar a activity do menu
+            Intent menuActivity = new Intent(this, MainMenu.class);
+            startActivity(menuActivity);
 
 
         }else{
@@ -87,7 +86,7 @@ public class MainActivity extends Activity {
                         // The toggle is enabled
                         requestPermission();
                     } else {
-                        //requestPermission();
+                        //switchGPS.setEnabled(false);
                     }
                 }
             });
@@ -101,7 +100,7 @@ public class MainActivity extends Activity {
 
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        // BEGIN_INCLUDE(onRequestPermissionsResult)
+        // 5 - Tratar da resposta
         if (requestCode == myVariables.MY_GPS_AUTH) {
             if (grantResults.length == 1 && grantResults[0] == PackageManager.PERMISSION_DENIED) {
                 //utilizador negou permissão
@@ -128,76 +127,98 @@ public class MainActivity extends Activity {
                 AlertDialog alert = alertBuilder.create();
                 alert.show();
 
-                }else{
+                }else {
+
                         //permissão negada
                         switchGPS.setChecked(false);
-                        AlertDialog.Builder alertBuilder = new AlertDialog.Builder(this);
-                        alertBuilder.setCancelable(true);
-                        alertBuilder.setTitle("Localização Negada.");
-                        alertBuilder.setMessage("A aplicação não funciona sem a localização.\nTem a certeza que deseja continuar?.");
-                        alertBuilder.setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
-                            @Override
-                            public void onClick(DialogInterface dialog, int which) {
-                                Context context = getApplicationContext();
-                                CharSequence text = "Localização NEGADA";
-                                int duration = Toast.LENGTH_SHORT;
-                                Toast toast = Toast.makeText(context, text, duration);
-                                TextView v = (TextView) toast.getView().findViewById(android.R.id.message);
-                                v.setTextColor(Color.RED);
-                                toast.show();
-                            }
-                        }).show();
-                        alertBuilder.setNegativeButton(android.R.string.no, new DialogInterface.OnClickListener() {
-                            @Override
-                            public void onClick(DialogInterface dialog, int which) {
-                                ActivityCompat.requestPermissions(MainActivity.this,
-                                        new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
-                                        myVariables.MY_GPS_AUTH);
-                            }
-                        }).show();
+                    AlertDialog.Builder alertBuilder = new AlertDialog.Builder(this);
+                    alertBuilder.setCancelable(true);
+                    alertBuilder.setTitle("Localização Negada.");
+                    alertBuilder.setMessage("A aplicação não funciona sem a localização.\nTem a certeza que deseja continuar?");
+                    alertBuilder.setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            Context context = getApplicationContext();
+                            CharSequence text = "Localização NEGADA";
+                            int duration = Toast.LENGTH_SHORT;
+                            Toast toast = Toast.makeText(context, text, duration);
+                            TextView v = (TextView) toast.getView().findViewById(android.R.id.message);
+                            v.setTextColor(Color.RED);
+                            toast.show();
+                        }
+                    });
+                    alertBuilder.setNegativeButton(android.R.string.no, new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            ActivityCompat.requestPermissions(MainActivity.this,
+                                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                                    myVariables.MY_GPS_AUTH);
+                        }
+                    }).show();
+
 
                 }
 
             } else {
-                //TODO launch menu
+                Intent menuActivity = new Intent(this, MainMenu.class);
+                startActivity(menuActivity);
             }
         }
     }
 
         private void requestPermission(){
-
-            //permissão não foi dada, pedir permissão
-            if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.ACCESS_FINE_LOCATION)) {
-
-                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, myVariables.MY_GPS_AUTH);
-
-                /*AlertDialog.Builder alertBuilder = new AlertDialog.Builder(this);
-                alertBuilder.setCancelable(true);
-                alertBuilder.setTitle("Location is necessary.");
-                alertBuilder.setMessage("The app will not work without location.");
-                alertBuilder.setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                        ActivityCompat.requestPermissions(MainActivity.this,
-                                new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
-                                myVariables.MY_GPS_AUTH);
-                    }
-                }).show();
-            AlertDialog alert = alertBuilder.create();
-            alert.show();*/
-
-
-                Context context = getApplicationContext();
-                CharSequence text = Integer.toString(myVariables.MY_GPS_AUTH);
-                int duration = Toast.LENGTH_SHORT;
-
-                Toast toast = Toast.makeText(context, text, duration);
-                toast.show();
-
-            }else{
-                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, myVariables.MY_GPS_AUTH);
-            }
-
+            //4 - Pedir permissão
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, myVariables.MY_GPS_AUTH);
         }
+
+    @Override
+    protected void onRestart(){
+
+        super.onRestart();
+
+        if (android.os.Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            verifyPermitions();
+        }else{
+            switchGPS.setSaveEnabled(false);
+            Intent menuActivity = new Intent(this, MainMenu.class);
+            startActivity(menuActivity);
+        }
+
+
+    }
+
+    @Override
+    protected void onStart(){
+
+        super.onStart();
+
+        if (android.os.Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            verifyPermitions();
+        }else{
+            switchGPS.setSaveEnabled(false);
+            Intent menuActivity = new Intent(this, MainMenu.class);
+            startActivity(menuActivity);
+        }
+
+
+    }
+
+    @Override
+    protected void onResume(){
+
+        super.onResume();
+
+        if (android.os.Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            verifyPermitions();
+        }else{
+            switchGPS.setSaveEnabled(false);
+            Intent menuActivity = new Intent(this, MainMenu.class);
+            startActivity(menuActivity);
+        }
+
+
+    }
+
+
 
 }
--- app/src/main/java/mma/example/com/exemplomapa/MainMenu.java
+++ app/src/main/java/mma/example/com/exemplomapa/MainMenu.java
@@ -1,7 +1,17 @@
 package mma.example.com.exemplomapa;
 
-public class MainMenu {
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 
 
-
+    public class MainMenu extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE); //esconde o título da janela
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN); //remove barra de notificação
+        setContentView(R.layout.menu_main); //Colocar após os dois anteriores para evitar crashes
+    }
 }
