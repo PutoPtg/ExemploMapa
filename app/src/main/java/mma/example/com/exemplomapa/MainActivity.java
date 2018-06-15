f92a1e37d29f45326427c2f2d8068a8f170829d5
Manuel Amado <manuelamado@iclio.pt>
2018-06-15 14:07:42 +0100

    mapa integrado e toasts quase bons

--- app/src/main/java/mma/example/com/exemplomapa/MainActivity.java
+++ app/src/main/java/mma/example/com/exemplomapa/MainActivity.java
@@ -37,9 +37,10 @@ public class MainActivity extends Activity {
         setContentView(R.layout.activity_main); //Colocar após os dois anteriores para evitar crashes
 
         //inicializar variáveis
-        myVariables = new VariablesClass(0, 0, 0);
+        myVariables = new VariablesClass(0, 1, 2);
         switchGPS = (Switch) findViewById(R.id.switch1);
         switchSDC = (Switch) findViewById(R.id.switch2);
+
 
         //passo 1 - Verificar a versão do SDK
         if (android.os.Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
@@ -55,7 +56,8 @@ public class MainActivity extends Activity {
     private void verifyPermitions() {
 
         //passo 2 - Verificar permissões
-        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED && ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED
+                && ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) {
 
             //mensagem de toast que indica ao utilizador que a permissão foi dada
 
@@ -64,6 +66,7 @@ public class MainActivity extends Activity {
                 int duration = Toast.LENGTH_SHORT;
                 Toast toast = Toast.makeText(context, text, duration);
                 toast.show();
+
 
             //se sim, tem permissão e pode arrancar o mapa
 
@@ -74,51 +77,52 @@ public class MainActivity extends Activity {
 
 
         } else {
-            if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_DENIED) {
-                //não tem permissão, tem de ser pedida
-                switchGPS.setChecked(false);
-                if(shouldShowRequestPermissionRationale(Manifest.permission.ACCESS_FINE_LOCATION)) {
+            //se não, foi negada a ambos?
+            if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_DENIED
+                    && ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_DENIED) {
+                if(!shouldShowRequestPermissionRationale(Manifest.permission.ACCESS_FINE_LOCATION)&&
+                        !shouldShowRequestPermissionRationale(Manifest.permission.WRITE_EXTERNAL_STORAGE)) {
                     Context context = getApplicationContext();
-                    CharSequence text = "A aplicação não tem acesso à localização.\n Por favor ligue a localização para a aplicação funcionar.";
                     int duration = Toast.LENGTH_SHORT;
-                    Toast toast = Toast.makeText(context, text, duration);
+                    Toast toast = Toast.makeText(context, R.string.toast_permissions, duration);
                     toast.show();
                 }
-
-                switchGPS.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-                    public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                        if (isChecked) {
-                            // The toggle is enabled
-                            requestPermission(0);
-                        } else {
-                            //switchGPS.setEnabled(false);
-                        }
-                    }
-                });
-            }
-            if (ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_DENIED) {
-                //não tem permissão, tem de ser pedida
-                switchSDC.setChecked(false);
-                Context context = getApplicationContext();
-                CharSequence text = "A aplicação não pode escrever.\n Por favor ligue a escrita para a aplicação funcionar.";
-                int duration = Toast.LENGTH_SHORT;
-                Toast toast = Toast.makeText(context, text, duration);
-                toast.show();
-                switchSDC.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-                    public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                        if (isChecked) {
-                            // The toggle is enabled
-                            requestPermission(1);
-                        } else {
-                            //switchGPS.setEnabled(false);
-                        }
-                    }
-                });
             }
 
 
-        }
 
+                if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_DENIED) {
+                    //não tem permissão, tem de ser pedida
+                    switchGPS.setChecked(false);
+                    switchGPS.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+                        public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                            if (isChecked) {
+                               // The toggle is enabled
+                                requestPermission(0);
+                            }
+                        }
+                    });
+                }else {
+                    switchGPS.setChecked(true);
+                    switchGPS.setEnabled(false);
+                }
+
+                if (ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_DENIED) {
+                    //não tem permissão, tem de ser pedida
+                    switchSDC.setChecked(false);
+                    switchSDC.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+                        public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                            if (isChecked) {
+                                // The toggle is enabled
+                                requestPermission(1);
+                            }
+                        }
+                    });
+                }else {
+                    switchSDC.setChecked(true);
+                    switchSDC.setEnabled(false);
+                }
+            }
     }
 
     @Override
@@ -182,66 +186,66 @@ public class MainActivity extends Activity {
 
                 }
 
-            } else {
-                if (requestCode == myVariables.MY_SD_AUTH) {
-                    if (grantResults.length == 1 && grantResults[0] == PackageManager.PERMISSION_DENIED) {
-                        //utilizador negou permissão
-                        boolean showRationale = ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.WRITE_EXTERNAL_STORAGE);
-                        if (!showRationale) {
-                            //permissão negada e não voltar a perguntar
-                            switchSDC.setChecked(false);
-                            switchSDC.setEnabled(false);
+            }
 
-                            AlertDialog.Builder alertBuilder = new AlertDialog.Builder(this);
-                            alertBuilder.setCancelable(true);
-                            alertBuilder.setTitle("Escrita Negada.");
-                            alertBuilder.setMessage("A aplicação não funciona sem a escrita.\nTerá de fornecer permissões nas opções a partir de agora.");
-                            alertBuilder.setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
-                                @Override
-                                public void onClick(DialogInterface dialog, int which) {
-                                    Intent intent = new Intent();
-                                    intent.setAction(Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
-                                    Uri uri = Uri.fromParts("package", getPackageName(), null);
-                                    intent.setData(uri);
-                                    startActivity(intent);
-                                }
-                            }).show();
-                            AlertDialog alert = alertBuilder.create();
-                            alert.show();
+        }else {
+            if (requestCode == myVariables.MY_SD_AUTH) {
+                if (grantResults.length == 1 && grantResults[0] == PackageManager.PERMISSION_DENIED) {
+                    //utilizador negou permissão
+                    boolean showRationale = ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.WRITE_EXTERNAL_STORAGE);
+                    if (!showRationale) {
+                        //permissão negada e não voltar a perguntar
+                        switchSDC.setChecked(false);
+                        switchSDC.setEnabled(false);
 
-                        } else {
+                        AlertDialog.Builder alertBuilder = new AlertDialog.Builder(this);
+                        alertBuilder.setCancelable(true);
+                        alertBuilder.setTitle("Escrita Negada.");
+                        alertBuilder.setMessage("A aplicação não funciona sem a escrita.\nTerá de fornecer permissões nas opções a partir de agora.");
+                        alertBuilder.setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                Intent intent = new Intent();
+                                intent.setAction(Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
+                                Uri uri = Uri.fromParts("package", getPackageName(), null);
+                                intent.setData(uri);
+                                startActivity(intent);
+                            }
+                        }).show();
+                        AlertDialog alert = alertBuilder.create();
+                        alert.show();
 
-                            //permissão negada
-                            switchSDC.setChecked(false);
-                            AlertDialog.Builder alertBuilder = new AlertDialog.Builder(this);
-                            alertBuilder.setCancelable(true);
-                            alertBuilder.setTitle("Escrita Negada.");
-                            alertBuilder.setMessage("A aplicação não funciona sem a Escrita.\nTem a certeza que deseja continuar?");
-                            alertBuilder.setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
-                                @Override
-                                public void onClick(DialogInterface dialog, int which) {
-                                    Context context = getApplicationContext();
-                                    CharSequence text = "Escrita NEGADA";
-                                    int duration = Toast.LENGTH_SHORT;
-                                    Toast toast = Toast.makeText(context, text, duration);
-                                    TextView v = (TextView) toast.getView().findViewById(android.R.id.message);
-                                    v.setTextColor(Color.RED);
-                                    toast.show();
-                                }
-                            });
-                            alertBuilder.setNegativeButton(android.R.string.no, new DialogInterface.OnClickListener() {
-                                @Override
-                                public void onClick(DialogInterface dialog, int which) {
-                                    ActivityCompat.requestPermissions(MainActivity.this,
-                                            new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},
-                                            myVariables.MY_SD_AUTH);
-                                }
-                            }).show();
-                        }
+                    } else {
 
+                        //permissão negada
+                        switchSDC.setChecked(false);
+                        AlertDialog.Builder alertBuilder = new AlertDialog.Builder(this);
+                        alertBuilder.setCancelable(true);
+                        alertBuilder.setTitle("Escrita Negada.");
+                        alertBuilder.setMessage("A aplicação não funciona sem a Escrita.\nTem a certeza que deseja continuar?");
+                        alertBuilder.setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                Context context = getApplicationContext();
+                                CharSequence text = "Escrita NEGADA";
+                                int duration = Toast.LENGTH_SHORT;
+                                Toast toast = Toast.makeText(context, text, duration);
+                                TextView v = (TextView) toast.getView().findViewById(android.R.id.message);
+                                v.setTextColor(Color.RED);
+                                toast.show();
+                            }
+                        });
+                        alertBuilder.setNegativeButton(android.R.string.no, new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                ActivityCompat.requestPermissions(MainActivity.this,
+                                        new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},
+                                        myVariables.MY_SD_AUTH);
+                            }
+                        }).show();
                     }
-                }
 
+                }
             }
 
         }
--- app/src/main/java/mma/example/com/exemplomapa/MainMenu.java
+++ app/src/main/java/mma/example/com/exemplomapa/MainMenu.java
@@ -1,17 +1,43 @@
 package mma.example.com.exemplomapa;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.Button;
+
+import mma.example.com.exemplomapa.MapActivity;
 
 
-    public class MainMenu extends Activity {
+public class MainMenu extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         requestWindowFeature(Window.FEATURE_NO_TITLE); //esconde o título da janela
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN); //remove barra de notificação
         setContentView(R.layout.menu_main); //Colocar após os dois anteriores para evitar crashes
+
+
+        final Button mapbutton = findViewById(R.id.map_btn);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+
+                Intent mapAct = new Intent(MainMenu.this, MapActivity.class);
+                startActivity(mapAct);
+            }
+        });
+
+        final Button dataButton = findViewById(R.id.map_btn);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+
+                Intent mapAct = new Intent(MainMenu.this, MapActivity.class);
+                startActivity(mapAct);
+            }
+        });
+
+
     }
 }
